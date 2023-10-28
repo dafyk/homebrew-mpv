cb14c48dbe037fe2d34ae2e88923fe90cf674aee
Da-FyK <dafyk@savero.net>
2023-10-28 17:26:00 +0200

    Added midnight commander and s-lang formulas

---
+++ Formula/midnight-commander.rb
@@ -0,0 +1,53 @@
+class MidnightCommander < Formula
+  desc "Terminal-based visual file manager"
+  homepage "https://www.midnight-commander.org/"
+  url "https://www.midnight-commander.org/downloads/mc-4.8.30.tar.xz"
+  mirror "https://ftp.osuosl.org/pub/midnightcommander/mc-4.8.30.tar.xz"
+  sha256 "5ebc3cb2144b970c5149fda556c4ad50b78780494696cdf2d14a53204c95c7df"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://ftp.osuosl.org/pub/midnightcommander/"
+    regex(/href=.*?mc[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  head do
+    url "https://github.com/MidnightCommander/mc.git", branch: "master"
+
+    depends_on "dafyk/mpv/autoconf" => :build
+    depends_on "dafyk/mpv/automake" => :build
+    depends_on "dafyk/mpv/libtool" => :build
+  end
+
+  depends_on "dafyk/mpv/pkg-config" => :build
+  depends_on "dafyk/mpv/glib"
+  depends_on "dafyk/mpv/libssh2"
+  depends_on "dafyk/mpv/openssl@3"
+  depends_on "dafyk/mpv/s-lang"
+
+  conflicts_with "minio-mc", because: "both install an `mc` binary"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --without-x
+      --with-screen=slang
+      --enable-vfs-sftp
+    ]
+
+    # Fix compilation bug on macOS 10.13 by pretending we don't have utimensat()
+    # https://github.com/MidnightCommander/mc/pull/130
+    ENV["ac_cv_func_utimensat"] = "no" if MacOS.version >= :high_sierra
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make", "install"
+
+    inreplace share/"mc/syntax/Syntax", Superenv.shims_path, "/usr/bin" if OS.mac?
+  end
+
+  test do
+  end
+end
---
+++ Formula/s-lang.rb
@@ -0,0 +1,28 @@
+class SLang < Formula
+  desc "Library for creating multi-platform software"
+  homepage "https://www.jedsoft.org/slang/"
+  url "https://www.jedsoft.org/releases/slang/slang-2.3.3.tar.bz2"
+  mirror "https://src.fedoraproject.org/repo/pkgs/slang/slang-2.3.3.tar.bz2/sha512/35cdfe8af66dac62ee89cca60fa87ddbd02cae63b30d5c0e3786e77b1893c45697ace4ac7e82d9832b8a9ac342560bc35997674846c5022341481013e76f74b5/slang-2.3.3.tar.bz2"
+  sha256 "f9145054ae131973c61208ea82486d5dd10e3c5cdad23b7c4a0617743c8f5a18"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "https://www.jedsoft.org/releases/slang/"
+    regex(/href=.*?slang[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "dafyk/mpv/libpng"
+
+  def install
+    png = Formula["dafyk/mpv/libpng"]
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-pnglib=#{png.lib}",
+                          "--with-pnginc=#{png.include}"
+    ENV.deparallelize
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+  end
+end
